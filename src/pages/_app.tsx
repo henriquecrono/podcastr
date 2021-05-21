639ef97345f4ac0f9e77823dc2ad3e94b8964802
Henrique Przybysz <henrique.miranda@virtuspay.com.br>
2021-05-20 22:10:57 -0300

    create style for app tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,20 @@
+import { Header } from '../components/Header';
+import { Player } from '../components/Player/index';
+
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+import styles from '../styles/app.module.scss';
 
-export default MyApp
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  )
+}
